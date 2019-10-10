60152447db42469dc45f4da5867f8abb7ed00325
David Smiles <davidugbero@gmail.com>
2019-10-10 11:51:20 +0100

    change primary_key back to the id property

--- models/store.py
+++ models/store.py
@@ -4,8 +4,8 @@ from db import db
 class StoreModel(db.Model):
     __tablename__ = 'stores'
 
-    id = db.Column(db.Integer)
-    name = db.Column(db.String(80), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
 
     items = db.relationship('ItemModel', lazy='dynamic')
 
