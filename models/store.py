824f57c333cd2eb8d62654e31949ac629bddb197
David Smiles <davidugbero@gmail.com>
2019-10-10 11:38:58 +0100

    update create table queries for item and store model

--- models/store.py
+++ models/store.py
@@ -4,8 +4,8 @@ from db import db
 class StoreModel(db.Model):
     __tablename__ = 'stores'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    id = db.Column(db.Integer)
+    name = db.Column(db.String(80), primary_key=True)
 
     items = db.relationship('ItemModel', lazy='dynamic')
 
